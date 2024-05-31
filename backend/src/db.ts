3e939af2592d19b2a84ba30ab148c805a8762028
Diya Kithani <diyakithani.2003@gmail.com>
2024-05-30 21:13:19 -0400

    Add mysql driver

---
+++ backend/src/db.ts
@@ -0,0 +1,8 @@
+import mysql from "mysql2";
+
+export const db = mysql.createConnection({
+    host: process.env.MYSQL_HOSTNAME,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+});
