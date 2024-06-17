8ed88298b6021db9e4fe8708fe095f3bb9916f6f
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-16 20:16:45 -0400

    Added comment feature to posts endpoint using parent_post_id

--- backend/src/routes/activity.ts
+++ backend/src/routes/activity.ts
@@ -24,8 +24,8 @@ router.post("/posts", async (req, res) => {
   }
   const db = await connect();
   const np = await db.query(
-    "insert into user_posts(user_id,content) values (? , ?)",
-    [req.session.uid, req.body.content]
+    "insert into user_posts(user_id,content, parent_post_id) values (? , ?, ?)",
+    [req.session.uid, req.body.content, req.body.parent_post_id ?? null]
   );
   res.json(np);
 });
