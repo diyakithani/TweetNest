9afc2cc3f823f585b74f707c9d4ba54c02ebd8ca
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-19 08:14:23 -0400

    Added Like Endpoint

--- backend/src/routes/activity.ts
+++ backend/src/routes/activity.ts
@@ -10,6 +10,7 @@ router.get("/posts", async (_req, res) => {
   );
   res.json(posts[0]);
 });
+
 //create post
 router.post("/posts", async (req, res) => {
   if (req.session.uid === undefined) {
@@ -28,6 +29,45 @@ router.post("/posts", async (req, res) => {
     [req.session.uid, req.body.content, req.body.parent_post_id ?? null]
   );
   res.json(np);
+});
+
+//like post endpoint
+router.put("/like", async (req, res) => {
+  if (req.session.uid == undefined) {
+    return res
+      .status(403)
+      .send("USER AUTHENTICATION SUS AF NGL ONGOD NOT COOL");
+  }
+  if (typeof req.body.post_id !== "number") {
+    return res.status(400).send("No post id");
+  }
+  const db = await connect();
+  const l = await db.query(
+    "select * from post_likes where user_id=? and post_id=?",
+    [req.session.uid, req.body.post_id]
+  );
+  if ((l[0] as []).length === 0) {
+    await db.query("insert into post_likes(post_id, user_id) values (?,?)", [
+      req.body.post_id,
+      req.session.uid,
+    ]);
+    return res.send(
+      req.session.uid + " liked " + req.body.post_id + " - post id"
+    );
+  } else {
+    await db.query("delete from post_likes where post_id=? and user_id=?", [
+      req.body.post_id,
+      req.session.uid,
+    ]);
+    return res
+      .status(200)
+      .send(
+        "POST UNLIKE KARDIYA HAI BY " +
+          req.session.uid +
+          " post id: " +
+          req.body.post_id
+      );
+  }
 });
 
 export default router;
