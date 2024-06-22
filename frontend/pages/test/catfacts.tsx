72775b1b14e8745fa550fdd5f9feeaa63bb2480c
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-22 14:36:02 -0400

    random cat generator api implemented

--- frontend/pages/test/catfacts.tsx
+++ frontend/pages/test/catfacts.tsx
@@ -1,14 +1,20 @@
 import { Blockquote, Container, Button } from '@mantine/core'
 import React from 'react'
+import axios from 'axios';
 
 export default function CatFacts() {
     const [quote, setQuote] = React.useState("Click the button to generate random cat facts");
+    async function generate_facts() {
+        const response = await axios.get("https://meowfacts.herokuapp.com/");
+        console.log(response);
+        setQuote(response.data.data[0]);
+    }
     return (
         <Container>
             <Blockquote>
                 {quote}
             </Blockquote>
-            <Button >Generate!</Button>
+            <Button onClick={generate_facts} >Generate!</Button>
         </Container>
     )
 }
