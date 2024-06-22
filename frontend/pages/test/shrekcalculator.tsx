dfdb9a19d1730cfcacdc22f22fa9e3bad69594f9
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-22 12:33:12 -0400

    all shrek calculator functionalities implemented

--- frontend/pages/test/shrekcalculator.tsx
+++ frontend/pages/test/shrekcalculator.tsx
@@ -1,4 +1,5 @@
-import { Avatar, Container, Grid, MantineStyleProp, MantineStyleProps, Text } from "@mantine/core";
+import { Avatar, Button, Container, Grid, MantineStyleProp, MantineStyleProps, Text, Title } from "@mantine/core";
+import { useState } from "react";
 
 export default function ShrekCalculator() {
 
@@ -17,26 +18,28 @@ export default function ShrekCalculator() {
         width: "80%"
     }
 
-    //state something
-
-
-
+    //state set karo
+    const [text, setText] = useState("");
+    const buttonClick = (sym: string) => {
+        if (sym === "=") {
+            return setText(eval(text));
+        }
+        setText(text + sym);
+    }
 
     const CalcButton = (txt: string) => (
-        <Avatar size="xl" component="button" radius="xl" variant="filled" color="#523213" style={{ margin: "auto", border: "1px solid black" }} >
+        <Avatar onClick={() => buttonClick(txt)} size="xl" component="button" radius="xl" variant="filled" color="#523213" style={{ margin: "auto", border: "1px solid black" }} >
             <Text c="#d3cca5" size="xl" variant="h1">{txt}</Text>
         </Avatar>
     );
 
     return (
         <Container>
-
             <Container className="display" style={displaystyle}>
-
+                <Title style={{ width: "100%", textAlign: "right" }}>{text}</Title>
             </Container>
             <Container className="buttons" style={buttonsStyle}>
-
-                <Grid gutter="xl" justify="center">
+                <Grid gutter="xl" justify="center" p="xl">
                     <Grid.Col span={3}>{CalcButton("7")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("8")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("9")}</Grid.Col>
@@ -44,7 +47,7 @@ export default function ShrekCalculator() {
                     <Grid.Col span={3}>{CalcButton("4")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("5")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("6")}</Grid.Col>
-                    <Grid.Col span={3}>{CalcButton("x")}</Grid.Col>
+                    <Grid.Col span={3}>{CalcButton("*")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("1")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("2")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("3")}</Grid.Col>
@@ -53,16 +56,10 @@ export default function ShrekCalculator() {
                     <Grid.Col span={3}>{CalcButton(".")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("+")}</Grid.Col>
                     <Grid.Col span={3}>{CalcButton("=")}</Grid.Col>
+                    <Grid.Col span={6}><Button onClick={() => setText("")} size="xl" c="#d3cca5" bg="#523213" w="80%" radius="xl" mx="xl">CLR</Button></Grid.Col>
+                    <Grid.Col span={6}><Button onClick={() => setText(text.slice(0, text.length - 1))} size="xl" c="#d3cca5" bg="#523213" w="80%" radius="xl" mx="xl">⌫</Button></Grid.Col>
                 </Grid>
-
-
             </Container>
-
-
-
         </Container >
-
     );
-
-
 }
