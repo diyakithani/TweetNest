000ada50c2c21fb954a3c9b32c4223813d0c0e4b
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-21 22:25:21 -0400

    Shinchan is Dancing and Not Dancing

---
+++ frontend/pages/test/diya.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+
+export default function diyaaaaaaaaaaa() {
+
+    const [dancingshinchan, setdancingshinchan] = useState(false);
+
+    useEffect(() => {
+        addEventListener("keydown", (ev) => {
+            console.log(ev.key);
+            if (ev.key == ' ') {
+                setdancingshinchan(true);
+
+            }
+        });
+        addEventListener("keyup", (ev) => {
+            console.log(ev.key);
+            if (ev.key == ' ') {
+                setdancingshinchan(false);
+            }
+        });
+    }, [])
+
+
+    if (dancingshinchan) {
+        return (
+            <div>
+                <img src="https://i.pinimg.com/originals/b3/55/b9/b355b9d38b108b906eed4ee36d7b31da.gif" height="500px" width="500px" />
+                <h1>shinchan is dancing</h1>
+            </div>
+        )
+    }
+    else {
+
+        return (
+            <div>
+                <img src="https://res.cloudinary.com/dzdgpwtox/image/upload/w_450,c_scale,f_auto/v1630928437/final_designs/seller_design_294646/f_20210906114036.png" height="500px" width="500px" />
+                <h1>shinchan is not dancing</h1>
+            </div>
+        );
+
+    }
+
+}
