ff3f56ad67621d737310bab8ed120ea23dc7a3d8
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-23 12:19:56 -0400

    useRef, useEffect in dancing ShinChan to optimize bg audio

--- frontend/pages/test/shinchan.tsx
+++ frontend/pages/test/shinchan.tsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function Shinchan() {
 
     const [dancingshinchan, setdancingshinchan] = useState(false);
+
+    const audio = useRef<HTMLAudioElement>(null);
 
     useEffect(() => {
         addEventListener("keydown", (ev) => {
@@ -22,25 +24,22 @@ export default function Shinchan() {
         addEventListener("touchend", () => setdancingshinchan(false));
     }, [])
 
+    useEffect(() => {
+        if (dancingshinchan) {
+            audio.current?.play();
+        }
+        else {
+            audio.current?.pause();
+        }
+    }, [dancingshinchan])
 
-    if (dancingshinchan) {
-        return (
-            <div>
-                <audio autoPlay src="/assets/shinchan_theme_song.mp3" />
-                <img src="https://i.pinimg.com/originals/b3/55/b9/b355b9d38b108b906eed4ee36d7b31da.gif" height="500px" width="500px" />
-                <h1>shinchan is dancing</h1>
-            </div>
-        )
-    }
-    else {
+    return (
+        <div>
+            <audio ref={audio} src="/assets/shinchan_theme_song.mp3" />
+            <img src={dancingshinchan ? "/assets/shinchan_dancing.gif" : "/assets/shinchan_not_dancing.png"} height="500px" width="500px" />
+            <h1>shinchan is {dancingshinchan ? "" : "not"} dancing</h1>
+        </div>
+    )
 
-        return (
-            <div>
-                <img src="https://res.cloudinary.com/dzdgpwtox/image/upload/w_450,c_scale,f_auto/v1630928437/final_designs/seller_design_294646/f_20210906114036.png" height="500px" width="500px" />
-                <h1>shinchan is not dancing</h1>
-            </div>
-        );
-
-    }
 
 }
