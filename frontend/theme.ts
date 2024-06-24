17f0e47acfb665eea11d7e24b74cc878fe5c2fed
Diya Kithani <diyakithani.2003@gmail.com>
2024-06-24 08:24:40 -0400

    theme implemented

--- frontend/theme.ts
+++ frontend/theme.ts
@@ -1,5 +1,21 @@
-import { createTheme } from '@mantine/core';
+import { MantineColorsTuple, createTheme } from '@mantine/core';
+
+const themeColor: MantineColorsTuple = [
+  '#effee7',
+  '#e0f8d4',
+  '#c2efab',
+  '#a2e67e',
+  '#87de57',
+  '#75d940',
+  '#6bd731',
+  '#59be23',
+  '#4da91b',
+  '#3d920c',
+];
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: 'themeColor',
+  colors: {
+    themeColor,
+  },
 });
