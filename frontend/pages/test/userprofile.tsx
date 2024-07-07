9f2d4673a5231cc9335ca83fa06ea33caf28403b
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-06 23:49:29 -0400

    user profile page draft

---
+++ frontend/pages/test/userprofile.tsx
@@ -0,0 +1,18 @@
+import { GridComp } from '@/components/UserProfilePage/gridcomp'
+import { Navbar } from '@/components/UserProfilePage/navbar'
+import React from 'react'
+
+function userprofile() {
+
+    return (
+        <div>
+            <Navbar></Navbar>
+            <GridComp></GridComp>
+
+
+
+        </div>
+    )
+}
+
+export default userprofile
