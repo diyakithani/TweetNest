cb8e192749d2d277cee2dd2507c6c3e44ca52600
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-09 17:32:01 -0400

    USERCARD UPDATED

--- frontend/components/UserProfilePage/usercard.tsx
+++ frontend/components/UserProfilePage/usercard.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Avatar, Center, Text, Group, Box, Paper } from '@mantine/core'
+
+import React from 'react';
+import { Avatar, Center, Text, Group, Box, Paper, Container } from '@mantine/core';
+import client from '@/utils/httpclient';
+
 const stats = [
     { value: '34K', label: 'Followers' },
     { value: '187', label: 'Follows' },
     { value: '1.6K', label: 'Posts' },
 ];
-
 function Usercard() {
     const items = stats.map((stat) => (
         <div key={stat.label}>
@@ -17,6 +19,13 @@ function Usercard() {
             </Text>
         </div>
     ));
+
+    const block = (title: string, value: string) =>
+        <Container my="md" display="flex">
+            <Text c="black" ta="left" fz="h4" m="sm">{title}</Text>
+            <Paper radius="xl" p="lg" bg="#D3D3D3" >{value}</Paper>
+        </Container>;
+
     return (
         <div>
             <Center bg="blue">
@@ -26,26 +35,15 @@ function Usercard() {
                     radius="md"
                 /> </Center>
             <Text size="lg" ta="center" c="black" bg="blue" fz="h3">BillieBogli123</Text> {/*TODO - how do i increase text size*/}
-            <Group bg="blue" mt="md" justify="center" gap={30} mt={0} p="sm">
+            <Group bg="blue" justify="center" gap={30} mt={0} p="sm">
                 {items}
             </Group>
             <Text c="black" ta="left" fz="h4" m="sm" style={{ fontSize: '1em', fontWeight: 'bold', color: 'black' }}>Name: <span style={{ fontSize: '1em', fontWeight: 'bold', color: 'black' }}>Billa Lover</span></Text>
-
-            <Text c="black" ta="left" fz="h4" m="sm">BIO: <Paper radius="xl" p="xl" bg="#D3D3D3">
-                <Text>Hi i have a raging god complex and I love to brag about myself,
-                    occasionally I like to eat worms.
-                </Text>
-            </Paper></Text>
-            <Text c="black" ta="left" fz="h4" m="sm">Joined:</Text>
-            <Text c="black" ta="left" fz="h4" m="sm">Born on:</Text>
-
-
-
-
-
-
+            {block("BIO: ", "Hi i have a raging god complex and I love to brag about myself, occasionally I like to eat worms.")}
+            {block("Joined: ", "13 September 2023")}
+            {block("Born on: ", "10 April 2002")}
         </div >
-    )
+    );
 }
 
-export default Usercard
+export default Usercard;
