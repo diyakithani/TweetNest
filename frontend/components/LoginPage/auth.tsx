150151c82ff7815448245c3a946071622cf5562b
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-09 23:11:38 -0400

    STATE BINDING USERNAME AND PASSWORD FOR LOGIN PAGE

--- frontend/components/LoginPage/auth.tsx
+++ frontend/components/LoginPage/auth.tsx
@@ -9,8 +9,26 @@ import {
     Anchor,
 } from '@mantine/core';
 import classes from './auth.module.css';
+import { useState } from 'react';
+import client from '@/utils/httpclient';
+import { useRouter } from 'next/router';
 
 export default function Auth() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+    async function login() {
+        const res = await client.post("/auth/login", { "username": username, "password": password });
+        if (res.status === 200) {
+            router.push("/home");
+        }
+        else {
+            setUsername("");
+            setPassword("");
+            alert("Invalid info bruhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
+        }
+
+    }
     return (
         <div className={classes.wrapper}>
             <Paper className={classes.form} radius={0} p={30}>
@@ -18,10 +36,10 @@ export default function Auth() {
                     Welcome back to Tweet-Nest!
                 </Title>
 
-                <TextInput label="Enter your Username" placeholder="ben_wormeater" size="md" />
-                <PasswordInput label="Enter your Password" placeholder="iloveworms" mt="md" size="md" />
+                <TextInput label="Enter your Username" placeholder="ben_wormeater" size="md" onChange={(event) => setUsername(event.currentTarget.value)} value={username} />
+                <PasswordInput label="Enter your Password" placeholder="iloveworms" mt="md" size="md" onChange={(event) => setPassword(event.currentTarget.value)} value={password} />
                 <Checkbox label="Keep me logged in" mt="xl" size="md" />
-                <Button fullWidth mt="xl" size="md">
+                <Button fullWidth mt="xl" size="md" onClick={login}>
                     Login
                 </Button>
 
