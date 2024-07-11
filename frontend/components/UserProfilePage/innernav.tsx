be5e6c1a2b5d182bcdfa8e8bdf00b1dddac143eb
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-10 20:59:07 -0400

    added inner nav for userprofile

---
+++ frontend/components/UserProfilePage/innernav.tsx
@@ -0,0 +1,71 @@
+import cx from 'clsx';
+import { useState } from 'react';
+import {
+    Container,
+    Avatar,
+    UnstyledButton,
+    Group,
+    Text,
+    Menu,
+    Tabs,
+    Burger,
+    rem,
+    useMantineTheme,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+    IconLogout,
+    IconHeart,
+    IconStar,
+    IconMessage,
+    IconSettings,
+    IconPlayerPause,
+    IconTrash,
+    IconSwitchHorizontal,
+    IconChevronDown,
+} from '@tabler/icons-react';
+
+import classes from './innernav.module.css';
+
+const user = {
+    name: 'Jane Spoonfighter',
+    email: 'janspoon@fighter.dev',
+    image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png',
+};
+
+const tabs = [
+    'Tweets',
+    'Comments',
+    'Liked Tweets'
+];
+
+export function Innernav() {
+    const theme = useMantineTheme();
+    const [opened, { toggle }] = useDisclosure(false);
+    const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+    const items = tabs.map((tab) => (
+        <Tabs.Tab value={tab} key={tab}>
+            {tab}
+        </Tabs.Tab>
+    ));
+
+    return (
+        <div className={classes.header}>
+            <Container size="md" bg="#add8e6">
+                <Tabs
+                    defaultValue="Home"
+                    variant="outline"
+                    visibleFrom="sm"
+                    classNames={{
+                        root: classes.tabs,
+                        list: classes.tabsList,
+                        tab: classes.tab,
+                    }}
+                >
+                    <Tabs.List>{items}</Tabs.List>
+                </Tabs>
+            </Container>
+        </div>
+    );
+}
