b3bd7a60c1b19c4b4877d12294785e2a07b12e47
Diya Kithani <diyakithani.2003@gmail.com>
2024-07-17 14:16:32 -0400

    Tweet and Home updated with get post logic

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,17 +1,21 @@
 import Tweet from '@/components/HomePage/Tweet'
 import CreatePost from '@/components/HomePage/createpost'
 import { Navbar } from '@/components/HomePage/navbar'
+import { useGetPosts } from '@/utils/hooks'
 import { Container } from '@mantine/core'
 import React from 'react'
 
 export default function Home() {
+
     return (
         <div>
             <Navbar></Navbar>
             <Container>
                 <CreatePost></CreatePost>
                 <Tweet></Tweet>
-                {[...Array(100)].map((e, i) => <Tweet />)}
+
+
+
             </Container>
 
 
