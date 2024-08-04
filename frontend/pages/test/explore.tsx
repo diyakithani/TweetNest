84cebe00417cfc4eb77d08d7f08c32033eaea0d0
Diya Kithani <diyakithani.2003@gmail.com>
2024-08-04 00:01:57 -0400

    Fixed explore as well :)

--- frontend/pages/test/explore.tsx
+++ frontend/pages/test/explore.tsx
@@ -1,23 +1,18 @@
 import { ExploreHeader } from '@/components/ExplorePage/exploreheader'
 import { Navbar } from '@/components/HomePage/navbar'
 import Tweet from '@/components/HomePage/Tweet'
+import { useGetPosts } from '@/utils/hooks'
 import { Container } from '@mantine/core'
 import React from 'react'
 
 function explore() {
+    const { posts, loading, error } = useGetPosts();
     return (
         <div>
             <Navbar></Navbar>
             <Container bg="lightgray" >
                 <ExploreHeader></ExploreHeader>
-                {[...Array(100)].map((e, i) => <Tweet post={{
-                    content: "explore",
-                    post_id: 0,
-                    parent_post_id: i - 1,
-                    media_path: "hehechan.jpg",
-                    timestamp: new Date(),
-                    user_id: 0
-                }} />)}
+                {posts.map((post) => <Tweet post={post}></Tweet>)}
             </Container>
 
         </div>
