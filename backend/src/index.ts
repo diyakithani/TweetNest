f29e9229ae971dd786255fecadb93af570c87526
Diya Kithani <diyakithani.2003@gmail.com>
2024-08-06 11:03:47 -0400

    configured uppy big time

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import test from "./routes/test";
 import auth from "./routes/auth";
 import activity from "./routes/activity";
 import cors from "cors";
-import companion from "@uppy/companion";
+import * as companion from "@uppy/companion";
 
 const app = express();
 
@@ -19,23 +19,41 @@ app.use(
     rolling: true,
   })
 );
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-); // Cross Origin Request Enabler
+const corsOptions: cors.CorsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
+app.use(cors(corsOptions)); // Cross Origin Request Enabler
 
 //uppy companion
-const { app: companionApp } = companion.app({
+const { app: companionApp, emitter } = companion.app({
   server: {
     host: process.env.HOST + ":" + process.env.PORT,
     protocol: "http",
     path: "/companion",
   },
   filePath: process.env.UPPY_FILE_PATH,
+  secret: process.env.SESSION_SECRET,
+  corsOrigins: corsOptions,
 });
 app.use("/companion", companionApp);
+
+// @ts-ignore
+emitter.on("upload-start", ({ token }) => {
+  console.log("Upload started", token);
+
+  // @ts-ignore
+  function onUploadEvent({ action, payload }) {
+    if (action === "success") {
+      emitter.off(token, onUploadEvent); // avoid listener leak
+      console.log("Upload finished", token, payload.url);
+    } else if (action === "error") {
+      emitter.off(token, onUploadEvent); // avoid listener leak
+      console.error("Upload failed", payload);
+    }
+  }
+  emitter.on(token, onUploadEvent);
+});
 
 // Augment express-session with a custom SessionData object
 declare module "express-session" {
