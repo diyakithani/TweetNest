973bea18bdde85bfbb123a5a65f6497011ee81c8
Diya Kithani <diyakithani.2003@gmail.com>
2024-08-06 11:32:38 -0400

    Revert "configured uppy companion in backend"
    This reverts commit 8299285d5fa1715c856671f58a24aa8707528dcb.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import test from "./routes/test";
 import auth from "./routes/auth";
 import activity from "./routes/activity";
 import cors from "cors";
-import * as companion from "@uppy/companion";
 
 const app = express();
 
@@ -19,41 +18,12 @@ app.use(
     rolling: true,
   })
 );
-const corsOptions: cors.CorsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-};
-app.use(cors(corsOptions)); // Cross Origin Request Enabler
-
-//uppy companion
-const { app: companionApp, emitter } = companion.app({
-  server: {
-    host: process.env.HOST + ":" + process.env.PORT,
-    protocol: "http",
-    path: "/companion",
-  },
-  filePath: process.env.UPPY_FILE_PATH,
-  secret: process.env.SESSION_SECRET,
-  corsOrigins: corsOptions,
-});
-app.use("/companion", companionApp);
-
-// @ts-ignore
-emitter.on("upload-start", ({ token }) => {
-  console.log("Upload started", token);
-
-  // @ts-ignore
-  function onUploadEvent({ action, payload }) {
-    if (action === "success") {
-      emitter.off(token, onUploadEvent); // avoid listener leak
-      console.log("Upload finished", token, payload.url);
-    } else if (action === "error") {
-      emitter.off(token, onUploadEvent); // avoid listener leak
-      console.error("Upload failed", payload);
-    }
-  }
-  emitter.on(token, onUploadEvent);
-});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+); // Cross Origin Request Enabler
 
 // Augment express-session with a custom SessionData object
 declare module "express-session" {
@@ -70,8 +40,6 @@ app.use("/activity", activity);
 
 // Start the server
 const port = process.env.PORT;
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Express app listening on port ${port}`);
 });
-
-companion.socket(server);
